c3bc3d55bc9dce3bda4f436ed8f38e54ef41a5ef
Maksim Ohvrill <589664@stud.hvl.no>
2024-04-19 14:31:15 +0200

    integration_database

--- src/firestore.py
+++ src/firestore.py
@@ -4,6 +4,8 @@ from google.oauth2 import service_account
 from google.cloud import firestore
 import json
 from decouple import config
+from datetime import datetime
+from frcm.datamodel.model import WeatherDataPoint, Location, WeatherData, Observations, Forecast
 
 def get_firestore_client():
     # Decode the base64-encoded credentials
@@ -20,56 +22,54 @@ def get_firestore_db():
     client = get_firestore_client()
     return client
 
-def save_data(location, date, risks):
+def save_data_to_db(location, date, risks, src):
     db = get_firestore_db()
-    doc_ref = db.collection('fire_risks').document(date)
+    doc_ref = db.collection('weather_data').document(date)
     data = {
         'observation_date': date,
+        'source': src,
         'latitude': location.latitude,
         'longitude': location.longitude,
-        'fire_risks': []
+        'weather_points': []
     }
     
     for risk in risks:
         fire_risk_data = {
             'date_time': risk.timestamp.isoformat(),
-            'risk_value': risk.ttf
+            'temperature': risk.temperature,
+            'humidity': risk.humidity,
+            'wind_speed': risk.wind_speed
         }
-        data['fire_risks'].append(fire_risk_data)
+        data['weather_points'].append(fire_risk_data)
     
     doc_ref.set(data)
-    print("Data saved successfully to Firestore!")
+    print("Data saved successfully")
 
-def get_date_firerisk(date):
-    """
-    Retrieve a single firerisk
-    """
+def get_date_weatherdata(date):
     db = get_firestore_db()
-    doc_ref = db.collection('fire_risks').document(date)
+    doc_ref = db.collection('weather_data').document(date)
     doc = doc_ref.get()
     if doc.exists:
-        print(f"Document data: {doc.to_dict()}")
-        return doc.to_dict()
+        doc_dict = doc.to_dict()
+        location = Location(latitude=doc_dict['latitude'], longitude=doc_dict['longitude'])
+        weather_data_points = [WeatherDataPoint(
+            temperature=dp['temperature'],
+            humidity=dp['humidity'],
+            wind_speed=dp['wind_speed'],
+            timestamp=datetime.fromisoformat(dp['date_time'])
+        ) for dp in doc_dict['weather_points']]
+        observations = Observations(source=doc_dict['source'], location=location, data=weather_data_points)
+        return observations
     else:
         print("No such document!")
     return None
 
-def get_all_firerisks():
-    """
-    Retrieve all firerisk
-    """
-    db = get_firestore_db()
-    docs = db.collection('fire_risks').stream()
-    for doc in docs:
-        print(f"{doc.id} => {doc.to_dict()}")
-    return [doc.to_dict() for doc in docs]
-
 def delete_firerisk(date):
     """
-    Delete a specific firerisk
+    Delete weatherdata by datespan
     """
     db = get_firestore_db()
-    db.collection('fire_risks').document(date).delete()
+    db.collection('weather_data').document(date).delete()
     print(f"Document {date} successfully deleted.")
 
 
